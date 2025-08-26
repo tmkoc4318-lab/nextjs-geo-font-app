1e69ada5fb4ee174774fc91f818073461ad1514d
tmkoc4318-lab <tmkoc4318@gmail.com>
2025-08-26 18:36:04 +0530

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1756213555355

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_ed254bc8._.js",
+  "static/chunks/node_modules_b605b999._.js"
+],
+    source: "dynamic"
+});
